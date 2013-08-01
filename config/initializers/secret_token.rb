6e18d24321749760ad3c0f5b28f5f83772bbd7ce
Anastasia Burchalova <a.burchalova@itransition.com>
2013-08-01 15:31:27 +0300

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Downloader::Application.config.secret_token = '19ef229b4b4bc47c9100e33eec367efe5c3e7de6f47855b352239ddd561d2ad8802c521724e644cdae3f28e3a8e5f43908207ee4374163ab187db67a14c1bf15'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Downloader::Application.routes.draw do
+  root :to => "home#index"
+end
